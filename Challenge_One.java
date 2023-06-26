6aa5aa3f9f2f075cc0cbb167dc7b73849aa53f6e
sairammatta <72718664+sairammatta@users.noreply.github.com>
2023-06-26 23:19:48 +0530

    Add files via upload
    Square root problem Challenge one accepted

---
+++ Challenge_One.java
@@ -0,0 +1,45 @@
+/*Java Challenge (1/20)
+---------------------
+
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.*/
+
+
+package week1.day2;
+
+public class Challenge_One {
+
+	public static void main(String[] args) {
+
+		System.out.println(squareroot(21));
+	}
+	
+	public static int squareroot(int a) {
+		int sqrt;
+		int val=a/2;
+		
+		do {
+			sqrt=val;
+			val=(sqrt+(a/sqrt))/2;
+		}while((sqrt-val)!=0) ;
+		
+		return sqrt;
+		
+	}
+
+}
